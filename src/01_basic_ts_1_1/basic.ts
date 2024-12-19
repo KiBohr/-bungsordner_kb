0314a09434d3e00447974bb7b39e42962bc66c58
KiBohr <katharinabohr@web.de>
2024-12-19 14:44:39 +0100

    basic files structure with exercices from 19.12.24

---
+++ src/01_basic_ts_1_1/basic.ts
@@ -0,0 +1,105 @@
+
+// * Intro-TS-Level-1_12            
+
+console.log("Welcome");
+
+let dogName = "Safran";
+console.log(dogName);
+
+dogName = "elf";
+
+let boolean1 = "vier";
+boolean1 = "4";
+console.log(boolean1);
+
+let seventeen = 2;
+
+const sum = seventeen + 44;
+console.log(sum);
+
+
+// *  Intro-TS-Level-1_13 (Operatoren)
+
+// Addition
+let addition_operator: number = 1 + 1;
+console.log('addition: ' + addition_operator);
+
+// Subtraktion
+let subtraktion_operator: number = 2 - 1;
+console.log('subtraktion: ' + subtraktion_operator);
+
+// Multiplikation
+let multiplication_operator: number = 2 * 2;
+console.log('multiplication: ' + multiplication_operator);
+
+// Division
+let division_operator: number = 4 / 2;
+console.log('division: ' + division_operator);
+
+// Modulo: zeigt an, was der Rest sein würde.
+let modulo_operator: number = 14 % 5;
+console.log('modulus: ' + modulo_operator);
+
+// * Intro-TS-Level-1_8 (Variablen)
+
+let x: number = 20;
+
+let y: number = 30;
+
+console.log(x + y);
+
+console.log(x - y);
+
+console.log(x * y);
+
+console.log(x / y);
+
+let z = 10;
+
+let resultOne = (x + y) - z;
+console.log(resultOne);
+
+let a = 15;
+
+let b = 9;
+
+console.log( a % b);
+
+let c = 20;
+
+let resultTwo = (a + b) * c;
+console.log(resultTwo);
+
+a++;
+console.log(a);
+
+b--;
+console.log(b);
+
+let resultThree = (a - b);
+console.log( resultThree % resultTwo);
+
+// * Intro-TS-Level-1_16 (Arithmetische Operatoren)
+
+let score: number = 5 + 5 * 10;
+console.log('result: ' + score);
+
+let score1 = (5 + 5) * 10;
+console.log('result: ' + score1);
+
+let score2 = 0;
+score2 = score2 + 10;
+console.log('result ' + score2);
+
+let zahl = 1;
+zahl = zahl + 1;
+zahl += 1;
+console.log(zahl);
+
+zahl++;
+console.log('increment: ' + zahl);
+
+zahl--;
+console.log('increment ' + zahl);
+
+
